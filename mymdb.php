9c461d63136a0ca9450cdd2b4c1d6d26838d8a3d
Shakhzodbek <shakhzodbek.isomiddinov@gmail.com>
2018-05-02 22:49:44 +0500

    First part - DONE

---
+++ mymdb.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="post">
+		<input type="text" name="first_name"> <input type="text" name="last_name">
+	</form>
+</body>
+</html>
